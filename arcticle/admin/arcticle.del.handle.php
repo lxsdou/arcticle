af5b213a84fb6a781119d5208230c2fb8d44d660
lxsdou <2991616932@qq.com>
2016-06-27 10:07:10 +0800

    Revert "Add files via upload"
    This reverts commit 799f396f70584b238b68245bfd088cbc44c27c4f.

--- arcticle/admin/arcticle.del.handle.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once('../connect.php');
-	$id = $_GET['id'];
-	$deletesql = "delete from arcticle where Id = $id";
-	mysql_query($deletesql); 
-?>
--- arcticle/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	header("Content-Type: text/html; charset=utf-8");
-	define('HOST', 'localhost');
-	define('USERNAME', 'root');
-	define('PASSWORD', 'usbw');
-?>
